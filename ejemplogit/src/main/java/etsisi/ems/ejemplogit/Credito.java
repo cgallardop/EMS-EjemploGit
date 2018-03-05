1a8e6b372661bbb003921379556114ed05b61890
jdiazfernandez <yesica.diaz@upm.es>
2018-03-05 23:10:23 +0100

    insert const for bank comission

--- ejemplogit/src/main/java/etsisi/ems/ejemplogit/Credito.java
+++ ejemplogit/src/main/java/etsisi/ems/ejemplogit/Credito.java
@@ -6,6 +6,7 @@ import java.time.LocalDate;
 public class Credito extends Tarjeta {
 	protected double mCredito;
 	protected Vector<Movimiento> mMovimientos;
+	static final double COMISION = 0.05;
 
 	public Credito(String numero, String titular, LocalDate fechaCaducidad, double credito) {
 		super(numero, titular, fechaCaducidad);
@@ -18,7 +19,7 @@ public class Credito extends Tarjeta {
 		m.setConcepto("Retirada en cuenta asociada (cajero automático)");
 		// x=(x*0.05<3.0 ? 3 : x*0.05); // Añadimos una comisión de un 5%, mínimo de 3 euros.
 		// m.setImporte(x);
-		double comision = (x * 0.05 < 3.0 ? 3 : x * 0.05); // Añadimos una comisión de un 5%, mínimo de 3 euros.
+		double comision = (x * COMISION < 3.0 ? 3 : x * COMISION); // Añadimos una comisión de un 5%, mínimo de 3 euros.
 		m.setImporte(x + comision);
 		mMovimientos.addElement(m);
 		if (x > getCreditoDisponible())
